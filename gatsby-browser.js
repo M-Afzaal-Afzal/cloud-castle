843cdf107df197dc903a09d183eb3ba6ac8826a6
M Afzaal Afzal <afzaal1721@gmail.com>
2022-05-01 22:43:44 +0500

    done with the slider with react slick

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,9 +7,11 @@
 import './src/styles/globals.css'
 
 // Import Swiper styles
-import "swiper/css";
-
-import "swiper/css/bundle";
+// import "swiper/css";
+//
+// import "swiper/css/bundle";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import React  from "react"
 import Layout from "./src/components/layout/layout";
--- src/components/homePage/CloudCastleGallerySection.js
+++ src/components/homePage/CloudCastleGallerySection.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import {ResponsiveBody1, ResponsiveH1} from "../common/typography";
-import {Swiper, SwiperSlide} from "swiper/react";
-import img from '../../images/slider/1.png'
+// import {Swiper, SwiperSlide} from "swiper/react";
 import {StaticImage} from "gatsby-plugin-image";
+
+import Slider from "react-slick";
+
+const settings = {
+    className: "slider variable-width",
+    // dots: true,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 1,
+    swipeToSlide: true,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: false,
+};
+
 
 const slidesData = [
     {
@@ -36,184 +50,190 @@ export const CloudCastleGallerySection = () => {
 
                 {/*     Carousel    */}
                 <div className={`mt-12`}>
-                    <Swiper
-                        loop={true}
-                        slidesPerView={"auto"}
-                        spaceBetween={30}
-                        className="mySwiper"
-                    >
+
+                    <Slider {...settings}>
+                        {/*     First Slide     */}
+                        <div
+                            className={`px-3`}
+                            style={{
+                                maxWidth: '400px',
+                                // width: '100%',
+                            }}
+                        >
+                            <div className={`relative`}>
+
+                                <StaticImage width={400} alt={'slider img'} src={'../../images/slider/1.png'}/>
+
+                                {/*    top overlay  */}
+                                <dilv
+                                    className={`absolute w-[355px] z-50 bg-[#04001680] bottom-8 left-1/2 -translate-x-1/2 p-4 flex flex-row justify-between rounded-lg gap-4`}>
+                                    {/*     OWNERSHIP*/}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Ownership
+                                        </ResponsiveBody1>
+                                        <ResponsiveH1
+                                            hasCustomFontSize={true}
+                                            className={`text-[16px] sm:text-[20px] tracking-normal lg:text-[22px]`}>
+                                            25%
+                                        </ResponsiveH1>
+                                    </div>
+
+                                    {/*     VACATION    */}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Vacations
+                                        </ResponsiveBody1>
+                                        <div className={`flex items-center gap-2`}>
+                                            <ResponsiveH1
+                                                hasCustomFontSize={true}
+                                                className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
+                                                25%
+                                            </ResponsiveH1>
+                                            <ResponsiveBody1 className={`shrink-0 text-sm`}>
+                                                per year
+                                            </ResponsiveBody1>
+                                        </div>
+                                    </div>
+
+                                    {/*     LOCATION    */}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Location
+                                        </ResponsiveBody1>
+                                        <ResponsiveH1
+                                            hasCustomFontSize={true}
+                                            className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
+                                            Jupitar
+                                        </ResponsiveH1>
+                                    </div>
+                                </dilv>
+                            </div>
+                        </div>
 
                         {/*     First Slide     */}
-                        <SwiperSlide
+                        <div
+                            className={`px-3`}
                             style={{
                                 maxWidth: '400px',
                                 // width: '100%',
-                                position: 'relative',
                             }}
                         >
-                            <StaticImage alt={'slider img'} src={'../../images/slider/1.png'}/>
+                            <div className={`relative`}>
 
-                            {/*    top overlay  */}
-                            <div
-                                className={`absolute w-[90%] z-50 bg-[#04001680] bottom-8 left-1/2 -translate-x-1/2 p-4 flex flex-row justify-between rounded-lg gap-4`}>
-                                {/*     OWNERSHIP*/}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Ownership
-                                    </ResponsiveBody1>
-                                    <ResponsiveH1
-                                        hasCustomFontSize={true}
-                                        className={`text-[16px] sm:text-[20px] tracking-normal lg:text-[22px]`}>
-                                        25%
-                                    </ResponsiveH1>
-                                </div>
+                                <StaticImage width={400} alt={'slider img'} src={'../../images/slider/1.png'}/>
 
-                                {/*     VACATION    */}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Vacations
-                                    </ResponsiveBody1>
-                                    <div className={`flex items-center gap-2`}>
+                                {/*    top overlay  */}
+                                <dilv
+                                    className={`absolute w-[375px] z-50 bg-[#04001680] bottom-8 left-1/2 -translate-x-1/2 p-4 flex flex-row justify-between rounded-lg gap-4`}>
+                                    {/*     OWNERSHIP*/}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Ownership
+                                        </ResponsiveBody1>
+                                        <ResponsiveH1
+                                            hasCustomFontSize={true}
+                                            className={`text-[16px] sm:text-[20px] tracking-normal lg:text-[22px]`}>
+                                            25%
+                                        </ResponsiveH1>
+                                    </div>
+
+                                    {/*     VACATION    */}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Vacations
+                                        </ResponsiveBody1>
+                                        <div className={`flex items-center gap-2`}>
+                                            <ResponsiveH1
+                                                hasCustomFontSize={true}
+                                                className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
+                                                25%
+                                            </ResponsiveH1>
+                                            <ResponsiveBody1 className={`shrink-0 text-sm`}>
+                                                per year
+                                            </ResponsiveBody1>
+                                        </div>
+                                    </div>
+
+                                    {/*     LOCATION    */}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Location
+                                        </ResponsiveBody1>
                                         <ResponsiveH1
                                             hasCustomFontSize={true}
                                             className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
-                                            25%
+                                            Jupitar
                                         </ResponsiveH1>
-                                        <ResponsiveBody1 className={`shrink-0 text-sm`}>
-                                            per year
-                                        </ResponsiveBody1>
                                     </div>
-                                </div>
-
-                                {/*     LOCATION    */}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Location
-                                    </ResponsiveBody1>
-                                    <ResponsiveH1
-                                        hasCustomFontSize={true}
-                                        className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
-                                        Jupitar
-                                    </ResponsiveH1>
-                                </div>
+                                </dilv>
                             </div>
-                        </SwiperSlide>
+                        </div>
 
-                        {/*     SECOND SLIDE    */}
-                        <SwiperSlide
+
+                        {/*     First Slide     */}
+                        <div
+                            className={`px-3`}
                             style={{
                                 maxWidth: '400px',
                                 // width: '100%',
-                                position: 'relative',
                             }}
                         >
-                            <StaticImage alt={'slider img'} src={'../../images/slider/1.png'}/>
+                            <div className={`relative`}>
 
-                            {/*    top overlay  */}
-                            <div
-                                className={`absolute w-[90%] z-50 bg-[#04001680] bottom-8 left-1/2 -translate-x-1/2 p-4 flex flex-row justify-between rounded-lg gap-4`}>
-                                {/*     OWNERSHIP*/}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Ownership
-                                    </ResponsiveBody1>
-                                    <ResponsiveH1
-                                        hasCustomFontSize={true}
-                                        className={`text-[16px] sm:text-[20px] tracking-normal lg:text-[22px]`}>
-                                        25%
-                                    </ResponsiveH1>
-                                </div>
+                                <StaticImage width={400} alt={'slider img'} src={'../../images/slider/1.png'}/>
 
-                                {/*     VACATION    */}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Vacations
-                                    </ResponsiveBody1>
-                                    <div className={`flex items-center gap-2`}>
+                                {/*    top overlay  */}
+                                <dilv
+                                    className={`absolute w-[375px] z-50 bg-[#04001680] bottom-8 left-1/2 -translate-x-1/2 p-4 flex flex-row justify-between rounded-lg gap-4`}>
+                                    {/*     OWNERSHIP*/}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Ownership
+                                        </ResponsiveBody1>
+                                        <ResponsiveH1
+                                            hasCustomFontSize={true}
+                                            className={`text-[16px] sm:text-[20px] tracking-normal lg:text-[22px]`}>
+                                            25%
+                                        </ResponsiveH1>
+                                    </div>
+
+                                    {/*     VACATION    */}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Vacations
+                                        </ResponsiveBody1>
+                                        <div className={`flex items-center gap-2`}>
+                                            <ResponsiveH1
+                                                hasCustomFontSize={true}
+                                                className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
+                                                25%
+                                            </ResponsiveH1>
+                                            <ResponsiveBody1 className={`shrink-0 text-sm`}>
+                                                per year
+                                            </ResponsiveBody1>
+                                        </div>
+                                    </div>
+
+                                    {/*     LOCATION    */}
+                                    <div>
+                                        <ResponsiveBody1 className={`-mb-2 text-sm`}>
+                                            Location
+                                        </ResponsiveBody1>
                                         <ResponsiveH1
                                             hasCustomFontSize={true}
                                             className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
-                                            25%
+                                            Jupitar
                                         </ResponsiveH1>
-                                        <ResponsiveBody1 className={`shrink-0 text-sm`}>
-                                            per year
-                                        </ResponsiveBody1>
                                     </div>
-                                </div>
-
-                                {/*     LOCATION    */}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Location
-                                    </ResponsiveBody1>
-                                    <ResponsiveH1
-                                        hasCustomFontSize={true}
-                                        className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
-                                        Jupitar
-                                    </ResponsiveH1>
-                                </div>
+                                </dilv>
                             </div>
-                        </SwiperSlide>
+                        </div>
 
 
-                        {/*     THIRD SLIDE     */}
-                        <SwiperSlide
-                            style={{
-                                maxWidth: '400px',
-                                // width: '100%',
-                                position: 'relative',
-                            }}
-                        >
-                            <StaticImage alt={'slider img'} src={'../../images/slider/1.png'}/>
-
-                            {/*    top overlay  */}
-                            <div
-                                className={`absolute w-[90%] z-50 bg-[#04001680] bottom-8 left-1/2 -translate-x-1/2 p-4 flex flex-row justify-between rounded-lg gap-4`}>
-                                {/*     OWNERSHIP*/}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Ownership
-                                    </ResponsiveBody1>
-                                    <ResponsiveH1
-                                        hasCustomFontSize={true}
-                                        className={`text-[16px] sm:text-[20px] tracking-normal lg:text-[22px]`}>
-                                        25%
-                                    </ResponsiveH1>
-                                </div>
-
-                                {/*     VACATION    */}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Vacations
-                                    </ResponsiveBody1>
-                                    <div className={`flex items-center gap-2`}>
-                                        <ResponsiveH1
-                                            hasCustomFontSize={true}
-                                            className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
-                                            25%
-                                        </ResponsiveH1>
-                                        <ResponsiveBody1 className={`shrink-0 text-sm`}>
-                                            per year
-                                        </ResponsiveBody1>
-                                    </div>
-                                </div>
-
-                                {/*     LOCATION    */}
-                                <div>
-                                    <ResponsiveBody1 className={`-mb-2 text-sm`}>
-                                        Location
-                                    </ResponsiveBody1>
-                                    <ResponsiveH1
-                                        hasCustomFontSize={true}
-                                        className={`text-[16px] md:text-[20px] tracking-normal lg:text-[22px]`}>
-                                        Jupitar
-                                    </ResponsiveH1>
-                                </div>
-                            </div>
-                        </SwiperSlide>
+                    </Slider>
 
 
-                    </Swiper>
                 </div>
             </div>
         </div>
